e06e3a5ec31e2f7884673d5c9d3dbb3e2a46a1fa
Meiggin <781571871@qq.com>
2017-10-15 23:58:51 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+/*-------------弹出框--------------*/
+$('.case ul li').hover(function(){
+	$('.hidebox').show()
+},function(){
+	$('.hidebox').hide()
+})
+
+
+
+
 /*-----------------轮播图--------------------*/
 $(function() {
 	var num = 0;
@@ -55,11 +65,67 @@ $.ajax({
 	dataType: 'json',
 	success: function(data) {
 		for(var i = 0; i < 8; i++) {
-			var txt = data[i]['url'];
+			var lunbo = data[i]['url'];
 			$('.banner_bigImg').eq(i).attr({
-				'src': txt
+				'src': lunbo
 			});
 		}
 	}
 })
 /*------------------轮播图结束---------------------*/
+/*-------banneradvert数据-----------*/
+$.ajax({
+	url: 'http://localhost/jd/php/banner_advert.php',
+	dataType: 'json',
+	success: function(data) {
+		for(var i = 0; i < 8; i++) {
+			var banneradvert = data[i]['url'];
+			$('.advert_img').eq(i).attr({
+				'src': banneradvert
+			});
+		}
+	}
+})
+/*---------banneradvert结束-----*/
+/*-------------楼梯--------------*/
+//1.通过scroll事件，控制左侧的楼梯显示与隐藏
+	$(window).scroll(function(){
+		//获取scrolltop值
+		var $scrolltop=$(window).scrollTop();
+		console.log($scrolltop)
+		if($scrolltop>=1670){
+			$('#loutinav').fadeIn(500);//fadeIn();
+		}else{
+			$('#loutinav').fadeOut(500);
+		}
+		//4.触发滚轮，让对应的楼梯，添加类名，进行切换标注。
+		$('#loutinav li').each(function(){
+			var $top=$('.louti').eq($(this).index()).offset().top+400;
+			if($top>$scrolltop){
+				$('#loutinav li').removeClass('active');
+				$('#loutinav li').eq($(this).index()).addClass('active');
+				return false;
+			}
+		})
+		
+		
+	});
+	//2.点击楼梯，获取对应的楼层的top，将top赋值给(html,body)下面scrolltop
+	//on(events,[selector],[data],fn)
+	$('#loutinav li').not('.last').on('click',function(){
+		$(this).addClass('active').siblings().removeClass('active');
+		var $index=$(this).index();//当前的索引
+		var $top=$('.louti').eq($index).offset().top;//当前楼梯对应的楼层的top值
+		$('html,body').animate({//赋值 
+			//document.documentElement.scrollTop || document.body.scrollTop
+			scrollTop:$top
+		},200);
+	});
+	//3.回到顶部
+	$('.last').on('click',function(){
+		$('html,body').animate({//赋值 
+			//document.documentElement.scrollTop || document.body.scrollTop
+			scrollTop:0
+		});
+	});
+/*--------------楼梯结束----------------*/
