39ebfbfccd74d251d2709912b58dfb4ff6a96c4f
Meiggin <781571871@qq.com>
2017-10-17 23:04:15 +0800

    Add files via upload

---
+++ php/details.php
@@ -0,0 +1,17 @@
+<?php
+	header('content-type:text/html;charset=utf-8');
+	header("Access-Control-Allow-Origin", "*");  
+    header("Access-Control-Allow-Methods", "POST");  
+    header("Access-Control-Allow-Headers", "x-requested-with,content-type");
+	$conn=mysql_connect('localhost','root','');
+	mysql_select_db('tunbotu');
+	mysql_query('SET NAMES UTF8');
+	
+	$query='select * from  jingleft';
+	$result=mysql_query($query);
+	$arr=array();
+	for($i=0;$i<mysql_num_rows($result);$i++){
+		$arr[$i]=mysql_fetch_array($result);
+	}
+	echo json_encode($arr);
+?>
